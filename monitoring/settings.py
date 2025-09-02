7d108a91242270fc7625d196c47f270c5384c965
Juan Andrés Avelino Ospina <ja.avelino@uniandes.edu.co>
2025-09-01 21:42:28 -0500

    Change database settings to use static values
    Updated database configuration with hardcoded values.

--- monitoring/settings.py
+++ monitoring/settings.py
@@ -78,11 +78,11 @@ WSGI_APPLICATION = 'monitoring.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USERNAME'),
-        'PASSWORD': os.environ.get('DB_USERNAME_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
+        'NAME': 'monitoring_db',
+        'USER': 'monitoring_user',
+        'PASSWORD': 'isis2503',
+        'HOST': 'DB_HOST',
+        'PORT': '5432',
     }
 }
 
