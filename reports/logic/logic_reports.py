cfe41c3453b1e1b03921ff84b2bbb684395d771f
jorozcor02 <81937604+jorozcor02@users.noreply.github.com>
2026-04-09 20:57:21 -0500

    Update logic_reports.py

--- reports/logic/logic_reports.py
+++ reports/logic/logic_reports.py
@@ -1,7 +1,7 @@
 from ..models import Reports 
 
 def get_reports():
-    queryset = Reports.objects.all().order_by('-dateTime')[:10]
+    queryset = Reports.objects.all().order_by('-created_at')[:10]
     return (queryset)
 
 def create_report(form):
